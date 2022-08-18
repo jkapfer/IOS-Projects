82d7f0af5cf1ebf6bbb900eb78eca177717bb592
Jacob Kapfer <45276998+jkapfer@users.noreply.github.com>
2022-08-18 08:58:18 -0500

    ummm test
    yes

---
+++ Playgrounds/Playgrounds/xMyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print("Apple sux")
+print("OK but like how tf am i supposed to setup version control on this thing")
+print("I hate mac keyboard")
+let hitlist = ["Gabe", "Phil", "Robo Bell"]
+for x in hitlist {
+        print(x)
+}
+
